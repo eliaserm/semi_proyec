e5c772f5fe33c7e183cf594a9fceb40800a448ae
unknown <you@example.com>
2019-04-07 17:17:22 -0500

    semipyoyecto_12

---
+++ myApp/src/pages/buscar/buscar.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InfoPage } from '../info/info';
+
+/**
+ * Generated class for the BuscarPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-buscar',
+  templateUrl: 'buscar.html',
+})
+export class BuscarPage {
+  lista = [];
+  casas = [];
+  infoPage = InfoPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.lista = this.navParams.get('casas');
+  }
+  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BuscarPage');
+  }
+
+  getItems(ev: any) {
+    console.log(ev.target.value);
+    let colonial = ev.target.value.toLowerCase();
+
+    this.casas = this.lista.filter(casa => {
+      return casa.colonia.toLowerCase().includes(colonial)
+    });
+    console.log(JSON.stringify(this.casas));
+  }
+
+  info(casa){
+    this.navCtrl.push(this.infoPage, {casa: casa});
+}
+
+}
---
+++ myApp/src/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { InfoPage } from '../info/info';
+import { BuscarPage } from '../buscar/buscar';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  casas =[];
+  infoPage = InfoPage;
+  buscarPage = BuscarPage;
+  constructor(public navCtrl: NavController,public http:HttpClient) {
+    this.http.get('/v1/casas/').subscribe(data =>{
+      this.casas=data['casas'];
+      console.log(JSON.stringify(this.casas));
+    },error => {
+      console.log(JSON.stringify(error));
+    });
+  }
+  info(casa){
+    console.log('click');
+    this.navCtrl.push(this.infoPage, {casa: casa});
+  }
+  search(){
+    this.navCtrl.push(this.buscarPage, {casas: this.casas});
+}
+
+}
